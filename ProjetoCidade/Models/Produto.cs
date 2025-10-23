29f83b33ef14d07a318712c5ce7e4d8faaee5d35
Gustavo <gustavooliveiracosta349@gmail.com>
2025-10-23 14:50:03 -0300

    Criando as models Usuario e Produto

---
+++ ProjetoCidade/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoCidade.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public decimal preco { get; set; }
+        public int quantidade { get; set; }
+    }
+}
---
+++ ProjetoCidade/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoCidade.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+    }
+}
