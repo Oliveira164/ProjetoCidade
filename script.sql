7091c8914d0f43332b273bebcd7de4744b38dbfd
Gustavo <gustavooliveiracosta349@gmail.com>
2025-10-23 14:42:00 -0300

    Criando o script do banco de dados no mysql e instalando o pacote do MySqlData

---
+++ script.sql
@@ -0,0 +1,18 @@
+create database bdprojeto;
+
+use bdprojeto;
+
+create table Usuario(
+id int primary key auto_increment,
+nome varchar(40) not null,
+email varchar(40) not null,
+senha varchar(40) not null
+);
+
+create table Produto(
+id int primary key auto_increment,
+nome varchar(40) not null,
+descricao varchar(80) not null,
+preco decimal(10,2) not null,
+quantidade int not null
+);
