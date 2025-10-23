78001914d999901612c81350a75e8b2dc0b9576c
Gustavo <gustavooliveiracosta349@gmail.com>
2025-10-23 15:23:55 -0300

    Criando os códigos dos Repositórios e criando a conexão com banco de dados

--- ProjetoCidade/Repositorio/ProdutoRepositorio.cs
+++ ProjetoCidade/Repositorio/ProdutoRepositorio.cs
@@ -1,9 +1,85 @@
 ﻿using MySql.Data.MySqlClient;
+using ProjetoCidade.Models;
 using System.Data;
 
 namespace ProjetoCidade.Repositorio
 {
-    public class ProdutoRepositorio
+    public class ProdutoRepositorio(IConfiguration configuration)
     {
+        // Declara um campo privado somente leitura para armazenar a string de conexão com o MySQL.
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        //METODO CADASTRAR PRODUTO
+        public void AdicionarProduto(Produto produto)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                var cmd = conexao.CreateCommand();
+                // Cria um novo comando SQL para inserir dados na tabela 'cliente'
+                cmd.CommandText = "INSERT INTO Produto (nome,descricao,preco,quantidade) VALUES (@nome, @descricao, @preco, @quantidade)";
+
+                // Adiciona um parâmetro para o nome, definindo seu tipo e valor
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = produto.nome;
+                // Adiciona um parâmetro para a descrição, definindo seu tipo e valor
+                cmd.Parameters.Add("@descricao", MySqlDbType.VarChar).Value = produto.descricao;
+                // Adiciona um parâmetro para o preço, definindo seu tipo e valor
+                cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = produto.preco;
+                // Adiciona um parâmetro para a quantidade, definindo seu tipo e valor
+                cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = produto.quantidade;
+
+                // Executa o comando SQL de inserção e retorna o número de linhas afetadas
+                cmd.ExecuteNonQuery();
+                // Fecha explicitamente a conexão com o banco de dados 
+                conexao.Close();
+
+            }
+        }
+
+
+        //Método buscar todos os produtos
+
+        public Produto ObterProduto(string nome)
+        {
+            // Cria uma nova instância da conexão MySQL dentro de um bloco 'using'.
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                // Abre a conexão com o banco de dados MySQL.
+                conexao.Open();
+                // Cria um novo comando SQL para selecionar todos os campos da tabela 'Produto' onde o campo 'nome' corresponde ao parâmetro fornecido.
+                MySqlCommand cmd = new("SELECT * FROM Produto WHERE nome = @nome", conexao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Nome', especificando o tipo como VarChar e utilizando o valor do parâmetro 'nome'.
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = nome;
+
+                // Executa o comando SQL SELECT e obtém um leitor de dados (MySqlDataReader). O CommandBehavior.CloseConnection garante que a conexão
+                // será fechada automaticamente quando o leitor for fechado.
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    // Inicializa uma variável 'produto' como null. Ela será preenchida se um produto for encontrado.
+                    Produto produto = null;
+                    // Lê a próxima linha do resultado da consulta. Retorna true se houver uma linha e false caso contrário.
+                    if (dr.Read())
+                    {
+                        // Cria uma nova instância do objeto 'Produto'.
+                        produto = new Produto
+                        {
+                            // Lê o valor da coluna "id" da linha atual do resultado, converte para inteiro e atribui à propriedade 'id' do objeto 'produto'.
+                            id = Convert.ToInt32(dr["id"]),
+                            // Lê o valor da coluna "nome" da linha atual do resultado, converte para string e atribui à propriedade 'nome' do objeto 'profuto'.
+                            nome = dr["nome"].ToString(),
+                            // Lê o valor da coluna "descricao" da linha atual do resultado, converte para string e atribui à propriedade 'descricao' do objeto 'produto'.
+                            descricao = dr["descricao"].ToString(),
+                            // Lê o valor da coluna "preco" da linha atual do resultado, converte para string e atribui à propriedade 'preco' do objeto 'produto'.
+                            preco = Convert.ToDecimal(dr["preco"]),
+                            // Lê o valor da coluna "quantidade" da linha atual do resultado, converte para string e atribui à propriedade 'quantidade' do objeto 'produto'.
+                            quantidade = Convert.ToInt32(dr["quantidade"])
+                        };
+                    }
+                    /* Retorna o objeto 'produto'. Se nenhum usuário foi encontrado com o email fornecido, a variável 'produto'
+                     permanecerá null e será retornado.*/
+                    return produto;
+                }
+            }
+        }
     }
 }
--- ProjetoCidade/Repositorio/UsuarioRepositorio.cs
+++ ProjetoCidade/Repositorio/UsuarioRepositorio.cs
@@ -1,9 +1,81 @@
 ﻿using MySql.Data.MySqlClient;
+using ProjetoCidade.Models;
 using System.Data;
 
 namespace ProjetoCidade.Repositorio
 {
-    public class UsuarioRepositorio
+    public class UsuarioRepositorio(IConfiguration configuration)
     {
+        // Declara um campo privado somente leitura para armazenar a string de conexão com o MySQL.
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        //METODO CADASTRAR USUARIO
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                var cmd = conexao.CreateCommand();
+                // Cria um novo comando SQL para inserir dados na tabela 'cliente'
+                cmd.CommandText = "INSERT INTO Usuario (nome, email,senha) VALUES (@nome, @email, @senha)";
+
+                //Adiciona um parâmetro para o nome, definindo seu tipo e valor
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = usuario.nome;
+                // Adiciona um parâmetro para o email, definindo seu tipo e valor
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = usuario.email;
+                // Adiciona um parâmetro para o senha, definindo seu tipo e valor
+                cmd.Parameters.Add("@senha", MySqlDbType.VarChar).Value = usuario.senha;
+
+                // Executa o comando SQL de inserção e retorna o número de linhas afetadas
+                cmd.ExecuteNonQuery();
+                // Fecha explicitamente a conexão com o banco de dados 
+                conexao.Close();
+
+            }
+        }
+
+
+        //Método buscar todos os usuários
+
+        public Usuario ObterUsuario(string email)
+        {
+            // Cria uma nova instância da conexão MySQL dentro de um bloco 'using'.
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                // Abre a conexão com o banco de dados MySQL.
+                conexao.Open();
+                // Cria um novo comando SQL para selecionar todos os campos da tabela 'Usuario' onde o campo 'Email' corresponde ao parâmetro fornecido.
+                MySqlCommand cmd = new("SELECT * FROM Usuario WHERE email = @email", conexao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Email', especificando o tipo como VarChar e utilizando o valor do parâmetro 'email'.
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                // Executa o comando SQL SELECT e obtém um leitor de dados (MySqlDataReader). O CommandBehavior.CloseConnection garante que a conexão
+                // será fechada automaticamente quando o leitor for fechado.
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    // Inicializa uma variável 'usuario' como null. Ela será preenchida se um usuário for encontrado.
+                    Usuario usuario = null;
+                    // Lê a próxima linha do resultado da consulta. Retorna true se houver uma linha e false caso contrário.
+                    if (dr.Read())
+                    {
+                        // Cria uma nova instância do objeto 'Usuario'.
+                        usuario = new Usuario
+                        {
+                            // Lê o valor da coluna "Id" da linha atual do resultado, converte para inteiro e atribui à propriedade 'Id' do objeto 'usuario'.
+                            id = Convert.ToInt32(dr["id"]),
+                            // Lê o valor da coluna "Nome" da linha atual do resultado, converte para string e atribui à propriedade 'Nome' do objeto 'usuario'.
+                            nome = dr["nome"].ToString(),
+                            // Lê o valor da coluna "Email" da linha atual do resultado, converte para string e atribui à propriedade 'Email' do objeto 'usuario'.
+                            email = dr["email"].ToString(),
+                            // Lê o valor da coluna "Senha" da linha atual do resultado, converte para string e atribui à propriedade 'Senha' do objeto 'usuario'.
+                            senha = dr["senha"].ToString()
+                        };
+                    }
+                    /* Retorna o objeto 'usuario'. Se nenhum usuário foi encontrado com o email fornecido, a variável 'usuario'
+                     permanecerá null e será retornado.*/
+                    return usuario;
+                }
+            }
+        }
     }
 }
